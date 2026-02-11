9da1a8eddc62ce5a3a5a1a19bac94f44b5905f7b
Andrea <a.pignieri@studenti.unina.it>
2026-02-11 19:24:31 +0100

    fix(imports): removed unused imports

--- server/src/test/java/com/dietiestates25/service/UserServiceTest.java
+++ server/src/test/java/com/dietiestates25/service/UserServiceTest.java
@@ -13,12 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
